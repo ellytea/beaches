0af7d5e7e42c162762a7acc63f245180ff849eee
elly torres <nallelyt4@gmail.com>
2018-12-18 18:07:11 -0700

    Render beaches on app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Beaches from './Beaches.js'
+import { beachCounties, beaches } from './data.js'
+
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      beaches: beaches
+    }
     
   }
 
@@ -12,7 +17,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Beachy Keen</h1>
-        <Beaches />
+        <Beaches beaches={this.state.beaches} /> 
       </div>
     )
   }
--- src/Beaches.js
+++ src/Beaches.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 class Beaches extends Component {
-  constructor() {
-  super();
-  this.state = {
-    
+  constructor(props) {
+    super();
     
   }
+
+
+
+  render () {
+    return (
+      <ul>
+      {
+        this.props.beaches.map((beach) => {
+          return (
+            <li>{beach.name}</li>
+          )
+        })
+      }
+      </ul>
+    )
   }
 }
 
