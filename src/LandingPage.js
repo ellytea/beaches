a2a88425f7cc4f4c83f9ec4e8b5dc3e5928476c3
Elizabeth Yeh <esnider1699@gmail.com>
2018-12-19 21:56:06 -0700

    Add style to LandingPage and initial grid layout of beaches

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -21,25 +21,12 @@ class LandingPage extends Component {
     if (this.state.landingPage) {
       return (
         <div className='landing-page'>
-          <div class="sun">
-            <div class="ray_box">
-              <div class="ray ray1"></div>
-              <div class="ray ray2"></div>
-              <div class="ray ray3"></div>
-              <div class="ray ray4"></div>
-              <div class="ray ray5"></div>
-              <div class="ray ray6"></div>
-              <div class="ray ray7"></div>
-              <div class="ray ray8"></div>
-              <div class="ray ray9"></div>
-              <div class="ray ray10"></div>
-            </div>
+          <div className='landing-details-section'>
+            <h1 className='landing-headline'>BEACHY KEEN</h1>
+            <p className='landing-subhead'>SOUTHERN CALIFORNIA BEACHES</p>
+            <button className='landing-button' 
+              onClick={this.hide}>Find Your Happy Place</button>
           </div>
-          <h1 className='headline'>BEACHY KEEN</h1>
-          <p className='subhead'>SO CAL BEACHES</p>
-            <button className='find-button' 
-              onClick={this.hide}>Find Your Happy Place
-            </button>
         </div>
       );
     } else {
