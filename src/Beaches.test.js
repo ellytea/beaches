d18a6afa2e5f91a962e16befd528c9af3917efca
elly torres <nallelyt4@gmail.com>
2019-01-02 20:27:09 -0700

    Add beachVisiblity test

--- src/Beaches.test.js
+++ src/Beaches.test.js
@@ -6,16 +6,18 @@ const beaches = [
   { "name": "Montaña de Oro State Beach",
     "county": "San Luis Obispo County",
     "image": "https://i.imgur.com/VdqbEDr.jpg" 
-  },
-  { "name": "Cayucos State Beach",
-    "county": "San Luis Obispo County",
-    "image": "https://i.imgur.com/u7gAUPB.png" 
   }];
 
-  const beach = { "name": "Montaña de Oro State Beach",
-    "county": "San Luis Obispo County",
-    "image": "https://i.imgur.com/VdqbEDr.jpg" 
-  };
+const beach = { "name": "Montaña de Oro State Beach",
+  "county": "San Luis Obispo County",
+  "image": "https://i.imgur.com/VdqbEDr.jpg" 
+};
+
+const beachCounties = [
+  {
+      "name": "San Luis Obispo County",
+      "beaches": [ "Montaña de Oro State Beach", "Cayucos State Beach", "Pismo State Beach", "William Randolph Hearst Memorial State Beach" ]
+  }];
 
 describe('Beaches', () => {
   let wrapper;
@@ -24,12 +26,20 @@ describe('Beaches', () => {
       <Beaches key= 'Beaches'/>
       )
     wrapper.setState({
-      beaches: beaches
+      beaches: beaches,
+      allBeaches: beaches,
+      beachCounties: beachCounties,
+      beachCard: beach
     })
-})
+  })
 
   it('should match the snapshot with all data passed in', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should set beachCard state to a beach', () => {
+    wrapper.find('.beach-card').simulate('click', { target: {innerText: 'MONTAÑA DE ORO STATE BEACH'} })
+    expect(wrapper.state('beachCard')).toEqual(beach);
   })
 
 })
